c77f111203ac28f79b84906396e149d0e39dbea2
Matheus Francisco <matheusf_francisco@hotmail.com>
2023-01-18 20:39:54 -0300

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+
+        try{
+            $num1 = 5;
+            $num2 = 10;
+            logger()->debug('Aqui é um log antes da soma!');
+    
+            $soma = $num1 + $num2;
+            logger()->info('A soma ficou: ', compact('soma'));
+    
+            $sub = $num1 - $num2;
+            logger()->warning('A subtração ficou:', compact('num1', 'num2', 'sub'));
+    
+            if ($sub < 0){
+                logger()->error('Valor negativo', compact('sub'));
+            }
+            return 'Hello World!';
+        }catch(Exception $e){
+            logger()->error($e);
+            abort(500);
+        }
+
+    }
+
+    public function soma($num1, $num2){
+        $soma = $num1 + $num2;
+        logger()->info('Soma Feita');
+
+        return $soma;
+    }
+}
